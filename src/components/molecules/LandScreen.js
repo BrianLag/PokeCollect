0f3780e191b6136aba96ada6725c57a8cda59036
Brian Lagaude <brian.lagaude@gmail.com>
2020-12-26 12:13:13 +0100

    feat: add input to choose the pokemon

--- src/components/molecules/LandScreen.js
+++ src/components/molecules/LandScreen.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Bannière from '../../images/151Pokemons.jpg'
+import Pokelist from './Pokelist'
 
 export default function Landscreen(props) {
-    
+    const [ valeur, setValeur ] = useState('')
+    const [ showPokemon, setShowPokemon ] = useState(false)
 
+    const pokeReturn = () => (
+        <Pokelist pokeNumber={valeur} />
+    )
+ 
     return (
-        <>
-            <p>Hello Choubidou</p>
-        </>
+        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+            <img src={Bannière} alt="Bannière" style={{ width: '100%' }} />
+                <label>
+                    Recherche ton Pokémon
+                    <input type="text" name="name" onChange={(e) => setValeur(e.target.value)} />
+                </label>
+                <button onClick={() => setShowPokemon(true)} />
+            {showPokemon === true ? <Pokelist pokeNumber={valeur}/> : <p> recommence</p> }
+        </ div>
     )
 }
