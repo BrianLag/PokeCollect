f75105d412d4b820125e1c69458122066cf74cdc
Brian Lagaude <brian.lagaude@gmail.com>
2020-12-26 12:33:50 +0100

    refactor: change pokemon onclick

--- src/components/molecules/LandScreen.js
+++ src/components/molecules/LandScreen.js
@@ -1,24 +1,32 @@
 import React, { useState } from 'react'
 import Bannière from '../../images/151Pokemons.jpg'
 import Pokelist from './Pokelist'
+import Pokeball from '../../images/pokeball.png'
 
 export default function Landscreen(props) {
-    const [ valeur, setValeur ] = useState('')
-    const [ showPokemon, setShowPokemon ] = useState(false)
+    const [valeur, setValeur] = useState('');
+    const [pokeChoice, setPokechoice] = useState('');
+    const [showPokemon, setShowPokemon] = useState(false);
 
     const pokeReturn = () => (
         <Pokelist pokeNumber={valeur} />
     )
- 
+
     return (
         <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
             <img src={Bannière} alt="Bannière" style={{ width: '100%' }} />
+            <div style={{ flexDirection: 'row' }}>
                 <label>
                     Recherche ton Pokémon
-                    <input type="text" name="name" onChange={(e) => setValeur(e.target.value)} />
                 </label>
-                <button onClick={() => setShowPokemon(true)} />
-            {showPokemon === true ? <Pokelist pokeNumber={valeur}/> : <p> recommence</p> }
+                <br/>
+                <input type="text" name="name" onChange={(e) => setValeur(e.target.value)} />
+
+                <button onClick={() => {setShowPokemon(true); setPokechoice(valeur)}} >
+                    <img src={Pokeball} style={{ width: 25, height: 25 }} alt="pokeball" />
+                </button>
+            </div>
+            {showPokemon === true ? <Pokelist pokeNumber={pokeChoice} /> : <p> aucun pokemon sélectionné</p>}
         </ div>
     )
 }
