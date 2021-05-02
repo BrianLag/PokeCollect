f4cd2390161c6bf6adf70e8e635f35be51847ee4
Brian Lagaude <brian.lagaude@gmail.com>
2021-05-02 17:18:52 +0200

    add structure

--- src/components/molecules/LandScreen.js
+++ src/components/molecules/LandScreen.js
@@ -2,27 +2,30 @@ import React, { useState } from 'react'
 import Bannière from '../../images/151Pokemons.jpg'
 import Pokelist from './Pokelist'
 import Pokeball from '../../images/pokeball.png'
+import { Pokedex } from './Pokedex';
+import './Pokedex.css'
 
 export default function Landscreen(props) {
     const [valeur, setValeur] = useState('');
     const [pokeChoice, setPokechoice] = useState('');
     const [showPokemon, setShowPokemon] = useState(false);
 
-    const pokeReturn = () => (
-        <Pokelist pokeNumber={valeur} />
-    )
-
     return (
         <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
             <img src={Bannière} alt="Bannière" style={{ width: '100%' }} />
+            <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <div className='wrapper' >
+                    <Pokedex />
+                </div>
+            </div>
             <div style={{ flexDirection: 'row' }}>
                 <label>
                     Recherche ton Pokémon
                 </label>
-                <br/>
+                <br />
                 <input type="text" name="name" onChange={(e) => setValeur(e.target.value)} />
 
-                <button onClick={() => {setShowPokemon(true); setPokechoice(valeur)}} >
+                <button onClick={() => { setShowPokemon(true); setPokechoice(valeur) }} >
                     <img src={Pokeball} style={{ width: 25, height: 25 }} alt="pokeball" />
                 </button>
             </div>
---
+++ src/components/molecules/Pokedex.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import './Pokedex.css'
+import { gql, useQuery } from '@apollo/client';
+import { Link } from 'react-router-dom';
+
+
+const PokeGQL = gql`
+    query samplePokeAPIquery {
+      pokemon_v2_pokemonspeciesname(limit: 10, where: {language_id: {_eq: 5}}) {
+        name
+        pokemon_species_id
+      }
+    }
+  `
+
+export function Pokedex(props) {
+  const [pokeNumber, setpokeNumber] = useState('')
+  const { loading, error, data } = useQuery(PokeGQL)
+
+
+  if (loading) return <p>Loading Launches</p>;
+  if (error) return <p>Error in fetching :(</p>;
+
+
+  return data.pokemon_v2_pokemonspeciesname.map(({ name, pokemon_species_id }) => (
+    // <Link onClick={() => setpokeNumber(pokemon_species_id)} to={{pathname:`/pokemon/${pokemon_species_id}`}} >
+      <div className="pokeBloc">
+        <p>
+          {pokemon_species_id}. {name}
+        </p>
+        <img className="pokeImage" src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon_species_id}.svg`} alt="pokeImage" />
+      </div>
+    // </Link> 
+  ));
+};
